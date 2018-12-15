8af7451c1ed58768c2fac7b0f0e19381f2b5b190
PCloherty <PCloherty-git@users.noreply.github.com>
2018-12-15 16:56:13 +0000

    hover box working
    changes need to be done to the location and size of the hidden box ID though

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
-var hiddenBox =document.getElementsByClassName("hiddenBox");
-var cRow7Col2 =document.getElementsByClassName("cRow7Col2")
-cRow7Col2.addEventListener()
+//var hiddenBox =document.getElementsByClassName("#hiddenBox");
+//var cRow7Col2 =document.getElementsByClassName("#cRow7Col2")
+//cRow7Col2.addEventListener()
+
+$("cRow7Col2").hover(function(){
+    $(hiddenBox).css("opacity", 1);
+    }, function(){
+    $(hiddenBox).css("opacity", 0);
+  });
